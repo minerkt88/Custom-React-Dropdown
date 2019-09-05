72a54d274d930fa880cdda8956213963d3031781
Kyle Miner <minerkt88@gmail.com>
2019-09-05 17:04:50 -0600

    Added arrow img and some more styling to make it rotate onClick

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,19 +1,29 @@
 import React,{ useState } from 'react';
+
+import arrow from './img/Keyboard-arrow-left-01.svg';
 import './Dropdown.css';
 
-const Dropdown = ({items}) => {
+const Dropdown = props => {
     const [isDropped,toggleIsDropped] = useState(false);
-    const toggleDrop = () => toggleIsDropped(!isDropped);
+    const toggleDrop = () => {
+        toggleIsDropped(!isDropped)
+        const arrowImg = document.getElementById('arrow');
+        let turn = isDropped ? '0deg' : '-90deg';
+        arrowImg.setAttribute('style',`transform:rotate(${turn})`)
+    };
 
-    const options = items.map((e,i) => {
+    const options = props.items.map((e,i) => {
         return (<p key={i}>{e}</p>)
         // Maybe I was missing the bind call
         // return (<button onClick={function.bind(null,argument)}>{e}</button>)
     });
 
     return (
-        <div className='dd-container'>
-            <button onClick={toggleDrop} className='btn'>{items[0]}</button>
+        <div className={'dd-container'}>
+            <button onClick={toggleDrop} className='btn'>
+                {props.items[0]}
+                <img src={arrow} alt='arrow' id='arrow'/>
+            </button>
             {isDropped ? <div className='dd-options'>{options}</div> : null}
         </div>
     )
