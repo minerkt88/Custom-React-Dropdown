c45ed8e7f6482cae0f164b487ba9012289d80469
Kyle Miner <minerkt88@gmail.com>
2019-09-05 18:49:40 -0600

    Dropdown save point

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Dropdown from './components/Dropdown'
 import Items from './components/DropdownItems'
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      {/* <Dropdown items={['Tim and Eric Awesome Show: Great Job!','Tom Goes To The Mayor','Decker']}/> */}
-      <Dropdown items={Items.KCshows}/>
+      <Dropdown options={Items.KCshows} />
     </div>
   );
 }
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,15 +5,20 @@ import './Dropdown.css';
 
 const Dropdown = props => {
     const [isDropped,toggleIsDropped] = useState(false);
+    const [isSelected,setIsSelected] = useState(props.options[0]);
+
     const toggleDrop = () => {
         toggleIsDropped(!isDropped)
         const arrowImg = document.getElementById('arrow');
         let turn = isDropped ? '0deg' : '-90deg';
         arrowImg.setAttribute('style',`transform:rotate(${turn})`)
     };
+    const selectItem = (item) => {
+        setIsSelected(item);
+    };
 
-    const options = props.items.map((e,i) => {
-        return (<p key={i}>{e}</p>)
+    const options = props.options.map((e,i) => {
+        return (<p key={i} onClick={selectItem.bind(null,e)}>{e}</p>)
         // Maybe I was missing the bind call
         // return (<button onClick={function.bind(null,argument)}>{e}</button>)
     });
@@ -21,7 +26,7 @@ const Dropdown = props => {
     return (
         <div className={'dd-container'}>
             <button onClick={toggleDrop} className='btn'>
-                {props.items[0]}
+                {isSelected}
                 <img src={arrow} alt='arrow' id='arrow'/>
             </button>
             {isDropped ? <div className='dd-options'>{options}</div> : null}
